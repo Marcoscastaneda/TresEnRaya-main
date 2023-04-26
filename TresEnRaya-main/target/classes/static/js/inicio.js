cb79e180b73d45b6500a61a17c6c102385960a69
Marcoscastaneda <131269981+Marcoscastaneda@users.noreply.github.com>
2023-04-26 16:06:38 +0000

    -Mejora pagina inicial sin terminar-

---
+++ TresEnRaya-main/target/classes/static/js/inicio.js
@@ -0,0 +1,11 @@
+var boton_iniciar = document.getElementById("boton_iniciar");
+
+boton_iniciar.addEventListener("click", prueba)
+
+function prueba(){
+    
+    var inicio = document.getElementById("inicio")
+
+    inicio.style.display = "none"
+
+}
