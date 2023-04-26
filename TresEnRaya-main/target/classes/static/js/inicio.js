e2f70baecba1c703855b9ab7db8dad3bfbbb02ac
Marcoscastaneda <131269981+Marcoscastaneda@users.noreply.github.com>
2023-04-26 16:50:56 +0000

    Boton de musica no funcional

--- TresEnRaya-main/target/classes/static/js/inicio.js
+++ TresEnRaya-main/target/classes/static/js/inicio.js
@@ -10,3 +10,21 @@ function prueba(){
     setTimeout(function () {inicio.style.display = "none"}, 700);
 
 }
+
+/* AÑADIMOS LA MUSICA */
+
+var v = document.getElementsByTagName("audio")[0];
+var sound = false;
+
+var boton = document.getElementById("botonaudio");
+boton.addEventListener("click", function(){
+   if (!sound) {
+       v.play();
+       document.getElementById("imgsonido").setAttribute('src', 'img/sonido_off.png');
+       sound = true;
+   } else {
+       v.pause();
+       document.getElementById("imgsonido").setAttribute('src', 'img/sonido_on.png');
+       sound = false;
+   }
+});
